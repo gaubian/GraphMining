7af0430c3c8cda757316f7cb7d0234dfa7d4dc4f
aubian <aubian@zamokv5.crans.org>
2020-02-07 10:48:14 +0100

    Correct algorithm

--- main.cc
+++ main.cc
@@ -19,29 +19,36 @@ vector<vector<int>> f(set<int> &V, vector<vector<int>> &G, int l) {
 set<int> g(vector<vector<int>> &G, int k, int s) {
     int n = G.size(), m = 0, best_i = -1;
     for(auto neighbours : G) m += neighbours.size();
-    double best_sc = ((double) m) / ((double) (n * (n - 1)));
-    set<int> V;
+    m /= 2;
+    double best_sc = ((double) m) / (((double) n) * ((double) n - 1));
+    set<int> V, seen;
     for(int i = 0; i < n; ++i) V.insert(i);
     auto cliques = f(V, G, k);
     vector<set<int>> which_cliques(n);
     for(int c = 0; c < cliques.size(); ++c)
 	for(int u : cliques[c]) which_cliques[u].insert(c);
     set<pair<int, int>> D;
-    for(int u = 0; u < n; ++u) D.insert(make_pair(cliques[u].size(), u));
+    for(int u = 0; u < n; ++u) D.insert(make_pair(which_cliques[u].size(), u));
     vector<int> order_removed;
     for(int i = n - 1; i >= s; --i) {
+	//cout << best_sc << endl;
 	int u = (*(D.begin())).second;
-	order_removed.push_back(u);
+	//cout << (*(D.rbegin())).first << ' ' << (*(D.begin())).first << endl;
 	D.erase(D.begin());
+	order_removed.push_back(u);
+	seen.insert(u);
+	for(int v : G[u]) if(seen.find(v) == seen.end()) m--;
 	for(int c : which_cliques[u]) for(int v : cliques[c]) if(u != v) {
 	    D.erase(make_pair(which_cliques[v].size(), v));
 	    which_cliques[v].erase(c);
 	    D.insert(make_pair(which_cliques[v].size(), v));
 	}
-	m -= G[u].size();
-	if(((double) m) / ((double) (n * (n - 1))) > best_sc) {
-	    best_sc = ((double) m) / ((double) (n * (n - 1)));
-	    best_i = n - 1 - i;
+	//cout << m << ' ' << i << endl;
+	double sc = ((double) m) / (((double) i) * ((double) i - 1));
+	//cout << i << ' ' << m << ' ' << sc << ' ' << best_sc << endl;
+	if(sc > best_sc) {
+	    best_sc = sc;
+	    best_i = n - i - 1;
 	}
     }
     for(int i = 0; i <= best_i; ++i) V.erase(order_removed[i]);
@@ -49,10 +56,13 @@ set<int> g(vector<vector<int>> &G, int k, int s) {
 }
 
 int main() {
-    int n, k, s, u, v;
-    cin >> n >> k >> s;
+    int n, k = 3, s = 10, u, v;
+    cin >> n;
     vector<vector<int>> G(n);
-    while(cin >> u >> v) G[min(u, v)].push_back(max(u, v));
+    while(cin >> u >> v) {
+	G[u].push_back(v);
+	G[v].push_back(u);
+    }
     set<int> se = g(G, k, s);
     for(int x : se) cout << x << endl;
 }
