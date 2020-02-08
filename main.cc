2b3cc227e8350e04340fcd748a0962f42ba164a6
gaubian <guillaume.aubian@gaubian.xyz>
2020-02-09 00:17:12 +0100

    Faster version

--- main.cc
+++ main.cc
@@ -2,12 +2,17 @@
 
 using namespace std;
 
-vector<vector<int>> f(set<int> &V, vector<vector<int>> &G, int l) {
+void date(string s) {
+    time_t result = time(nullptr);
+    cout << s << ' ' << asctime(localtime(&result)) << endl;
+}
+
+vector<vector<int>> f(const vector<int> &V, vector<vector<int>> &G, int l) {
     if(l == 0) return {{}};
     vector<vector<int>> ans;
     for(int u : V) {
-        set<int> I;
-	for(int v : G[u]) if(v > u && V.find(v) != V.end()) I.insert(v);
+	vector<int> I;
+    	for(int v : G[u]) if(binary_search(V.begin(), V.end(), v)) I.push_back(v);
     	for(vector<int> se : f(I, G, l-1)) {
 	    se.push_back(u);
 	    ans.push_back(se);
@@ -16,53 +21,63 @@ vector<vector<int>> f(set<int> &V, vector<vector<int>> &G, int l) {
     return ans;
 }
 
-set<int> g(vector<vector<int>> &G, int k, int s) {
+unordered_set<int> g(vector<vector<int>> &G, vector<vector<int>> &ori, int k, int s) {
     int n = G.size(), m = 0, best_i = -1;
     for(auto neighbours : G) m += neighbours.size();
     m /= 2;
     double best_sc = ((double) m) / (((double) n) * ((double) n - 1));
-    set<int> V, seen;
-    for(int i = 0; i < n; ++i) V.insert(i);
-    auto cliques = f(V, G, k);
-    vector<set<int>> which_cliques(n);
+    vector<int> V;
+    vector<bool> seen(n, false);
+    for(int i = 0; i < n; ++i) V.push_back(i);
+    date("startf");
+    auto cliques = f(V, ori, k);
+    date("endf");
+    vector<unordered_set<int>> revcliques(n);
     for(int c = 0; c < cliques.size(); ++c)
-	for(int u : cliques[c]) which_cliques[u].insert(c);
-    set<pair<int, int>> D;
-    for(int u = 0; u < n; ++u) D.insert(make_pair(which_cliques[u].size(), u));
+	for(int u : cliques[c]) revcliques[u].insert(c);
+    priority_queue<pair<int,int>> D;
+    for(int u = 0; u < n; ++u) D.push(make_pair(-revcliques[u].size(), u));
     vector<int> order_removed;
-    for(int i = n - 1; i >= s; --i) {
-	//cout << best_sc << endl;
-	int u = (*(D.begin())).second;
-	//cout << (*(D.rbegin())).first << ' ' << (*(D.begin())).first << endl;
-	D.erase(D.begin());
+    date("D");
+    for(int i = n; i >= s;) {
+	int u = D.top().second;
+	D.pop();
+	if(seen[u]) continue;
+	--i;
 	order_removed.push_back(u);
-	seen.insert(u);
-	for(int v : G[u]) if(seen.find(v) == seen.end()) m--;
-	for(int c : which_cliques[u]) for(int v : cliques[c]) if(u != v) {
-	    D.erase(make_pair(which_cliques[v].size(), v));
-	    which_cliques[v].erase(c);
-	    D.insert(make_pair(which_cliques[v].size(), v));
+	seen[u] = true;
+	for(int v : G[u]) if(!seen[v]) m--;
+	unordered_set<int> temp;
+	for(int c : revcliques[u]) for(int v : cliques[c]) if(u != v) {
+	    revcliques[v].erase(c);
+	    temp.insert(v);
 	}
-	//cout << m << ' ' << i << endl;
+	for(int v : temp) D.push(make_pair(-revcliques[v].size(), v));
 	double sc = ((double) m) / (((double) i) * ((double) i - 1));
-	//cout << i << ' ' << m << ' ' << sc << ' ' << best_sc << endl;
 	if(sc > best_sc) {
 	    best_sc = sc;
 	    best_i = n - i - 1;
 	}
     }
-    for(int i = 0; i <= best_i; ++i) V.erase(order_removed[i]);
-    return V;
+    unordered_set<int> VV(V.begin(), V.end());
+    for(int i = 0; i <= best_i; ++i) VV.erase(order_removed[i]);
+    return VV;
 }
 
 int main() {
-    int n, k = 3, s = 10, u, v;
+    int n, k = 3, s = 4, u, v;
     cin >> n;
-    vector<vector<int>> G(n);
+    vector<vector<int>> G(n), ori(n);
+    date("PARSE");
     while(cin >> u >> v) {
 	G[u].push_back(v);
 	G[v].push_back(u);
+	ori[min(u, v)].push_back(max(u, v));
     }
-    set<int> se = g(G, k, s);
+    for(int u = 0; u < G.size(); ++u) sort(ori[u].begin(), ori[u].end());
+    date("g");
+    unordered_set<int> se = g(G, ori, k, s);
+    date("END");
     for(int x : se) cout << x << endl;
 }
+
